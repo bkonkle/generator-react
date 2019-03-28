06b2850bda4280f66e70e7cf26c199dc31ebf87d
Brandon Konkle <brandon@konkle.us>
2019-03-28 09:55:34 -0600

    Fix one more default port

--- generators/web/static/scripts/dev.ts
+++ generators/web/static/scripts/dev.ts
@@ -32,7 +32,7 @@ export async function main () {
   const config = configModule.default
 
   const host = process.env.HOST || 'localhost'
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000
+  const port = process.env.PORT ? Number(process.env.PORT) : 4000
 
   const devServerConfig: Configuration = {
     noInfo: true,
